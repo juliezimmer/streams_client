249a4236dc4c71acf6e8337658903c3615739a01
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-04-13 16:39:40 -0500

    converted StreamList component to a class and added code to render a list of streams when fetched.

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+ 
+class StreamList extends React.Component {
+   componentDidMount () {
+      this.props.fetchStreams();
+   }
 
-const StreamList = () => {
-   return (
-      <div>StreamList</div>
-   );
-};
+   // This takes the list of streams and renders them to the screen.
+   renderList() {
+      return this.props.streams.map(stream => {
+         return (
+            <div className="item" key={stream.id}>
+               <i className="large middle aligned icon camera"></i>
+               <div className="content">
+                  {stream.title}
+                  <div className="description">{stream.description}</div>
+               </div>
+            </div>
+         );
+      });
+   }
 
-export default StreamList;
+   render(){
+      return (
+         <div>
+            <h2>Streams</h2>
+            <div className="ui celled list">{this.renderList()}</div>
+         </div>
+      );
+   }
+}
+// this makes the list of streams available inside the component as props.
+const mapStateToProps = state => {
+   return { streams: Object.values(state.streams)};
+}
+
+export default connect(
+   mapStateToProps, 
+   { fetchStreams }
+) (StreamList);
