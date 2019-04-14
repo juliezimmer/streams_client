e375fe11ac47d408a3d8ee97abe041d668fdf8e2
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-04-14 17:25:42 -0500

    refactored StreamEdit.js to work with reduxForm wrapped StreamForm

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from 'lodash'; // used with <StreamForm />
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 
 class StreamEdit extends React.Component {
@@ -8,19 +10,31 @@ class StreamEdit extends React.Component {
       this.props.fetchStream(this.props.match.params.id);
    }
 
+   onSubmit =  (formValues) => {
+      console.log(formValues);
+   }; 
+
    render (){
       if (!this.props.stream) {
          return <div>Loading...</div>;
       }
       return (
-         <div>{this.props.stream.title}</div>
+         <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm // lodash _.pick() is used with initialValues so that only title and description are passed to the StreamForm //
+               initialValues={_.pick(this.props.stream, 'title', 'description')}
+               onSubmit={this.onSubmit} />
+         </div>
       );
    }
    
 }
-
+// this helps get the initialValues for the form
 const mapStateToProps = (state, ownProps) => {
    return { stream: state.streams[ownProps.match.params.id]};
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(
+   mapStateToProps, 
+   { fetchStream, editStream }
+)(StreamEdit);
